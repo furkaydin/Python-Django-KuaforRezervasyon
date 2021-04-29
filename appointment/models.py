a2c892cd19bdb7757069c65975342f0fd4491cd1
furkaydin <furkay16@gmail.com>
2021-04-29 18:04:38 +0300

    ücüncü update

--- appointment/models.py
+++ appointment/models.py
@@ -17,7 +17,7 @@ class Category(models.Model):
     slug = models.SlugField()
     parent = models.ForeignKey('self',blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
-    update_ate = models.DateTimeField(auto_now=True)
+    update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
